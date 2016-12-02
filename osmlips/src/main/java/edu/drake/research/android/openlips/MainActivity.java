d4d852d30f717b40811d367e0756639d26a54f61
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-12-02 15:21:26 -0600

    Chore: refactor
    - Will start to use Google Maps instead of Mapzen
    - and Google libraries

---
+++ osmlips/src/main/java/edu/drake/research/android/openlips/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.drake.research.android.openlips;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.drake.research.android.osmlips.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

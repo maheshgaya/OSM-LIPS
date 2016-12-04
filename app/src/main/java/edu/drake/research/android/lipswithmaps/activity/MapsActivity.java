f3fd1d7a6819a4fe8159b6167825cda9a0ea0fa8
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-12-04 17:01:14 -0600

    Chore: Refactor Maps Activity

--- app/src/main/java/edu/drake/research/android/lipswithmaps/activity/MapsActivity.java
+++ app/src/main/java/edu/drake/research/android/lipswithmaps/activity/MapsActivity.java
@@ -2,6 +2,7 @@ package edu.drake.research.android.lipswithmaps.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,16 +12,27 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import edu.drake.research.android.lipswithmaps.R;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback{
     private GoogleMap mMap;
     private final LatLng DRAKE_UNIVERSITY = new LatLng(41.6013800, -93.6518900);
 
+    //Binding views
+    @BindView(R.id.toolbar) Toolbar mToolbar;
+
+    private void setupToolbar(){
+        //shows Action bar
+        setSupportActionBar(mToolbar);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_maps);
+        setContentView(R.layout.activity_maps);
+        ButterKnife.bind(this);
+        setupToolbar();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);

0e3108f81fa734c4507630786d95079d772d744a
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-12-04 16:08:59 -0600

    Feat: added Google Maps

--- app/src/main/java/edu/drake/research/android/openlips/activity/MainActivity.java
+++ app/src/main/java/edu/drake/research/android/openlips/activity/MainActivity.java
@@ -3,13 +3,65 @@ package edu.drake.research.android.openlips.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import edu.drake.research.android.osmlips.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private GoogleMap mMap;
+    private final LatLng DRAKE_UNIVERSITY = new LatLng(41.6013800, -93.6518900);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney, Australia, and move the camera.
+        mMap.addMarker(new MarkerOptions().position(DRAKE_UNIVERSITY).title("Drake University! Yay!"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(drakeUniversity));
+        setCameraPosition(DRAKE_UNIVERSITY);
+    }
+
+    /**
+     * Set the camera zoom
+     * @param latitude
+     * @param longitude
+     */
+    public void setCameraPosition(double latitude, double longitude){
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude, longitude))      // Sets the center of the map
+                .zoom(15)                   // Sets the zoom
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
+    }
+
+    /**
+     *
+     * @param position
+     */
+    public void setCameraPosition(LatLng position){
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(position)      // Sets the center of the map
+                .zoom(15)              // Sets the zoom
+                .build();              // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
     }
 }

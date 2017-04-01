c25d541fe59dbc920a4db3653eb365e9a052bfab
Mahesh Gaya <mmaheshgaya@gmail.com>
2017-04-01 09:17:01 -0500

    chore: some cleanup and commenting

--- backend/src/main/java/edu/drake/research/web/lipswithmaps/backend/AllReadingServlet.java
+++ backend/src/main/java/edu/drake/research/web/lipswithmaps/backend/AllReadingServlet.java
@@ -48,14 +48,25 @@ public class AllReadingServlet extends HttpServlet {
     public static final String CSV_TYPE = "csv";
     public static final String JSON_TYPE = "json";
     public static final String FORMAT = "format";
+
+    /**
+     * According to format requested output JSON or CSV
+     * Default is JSON
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doGet(HttpServletRequest req, final HttpServletResponse resp)
             throws ServletException, IOException {
         String parameter = JSON_TYPE;
         if (req.getParameterMap().containsKey(FORMAT)) {
+            //get the output format (CSV or JSON)
             parameter = req.getParameter(FORMAT);
         }
         //region Firebase Initialization
+        //Service account is found in the main/resources folder
         InputStream serviceAccount = this.getClass().getResourceAsStream("/service-account.json");
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredential(FirebaseCredentials.fromCertificate(serviceAccount))
@@ -71,20 +82,27 @@ public class AllReadingServlet extends HttpServlet {
         //endregion
 
         //region Firebase Query
-        // Read only access
+        //Firebase queries from a background thread,
+        // thus we need to await for the background thread to complete
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        // Read only access service account
         final DatabaseReference ref = FirebaseDatabase
                 .getInstance()
                 .getReference(Reading.TABLE_NAME);
-        final CountDownLatch countDownLatch = new CountDownLatch(1);
+
         final String formatParameter = parameter;
+
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                //Was always getting null value when referencing the data directly
-                //So decided to do it one by one with hardcoded strings
-                //TODO: Clean this up if can
+                //Output was always getting null value when referencing the data directly
+                //So decided to do it one by one with hardcoded key strings
+                //TODO: change the key string if the names of the variables are updated for the data
+
                 int childrenCount = (int) dataSnapshot.getChildrenCount();
-                int count = 1;
+
+                //return error if there is no content
                 if (childrenCount == 0) {
                     try {
                         String message = "{ \"error\": \"No data in the database\"}";
@@ -95,14 +113,20 @@ public class AllReadingServlet extends HttpServlet {
                     }
                 }
 
+                //keep a chronologically ordered Hash Map for the unique wifi bssid
+                //will be used for CSV Output, however, keep the structure for JSON
                 Map<String, String> uniqueWifisMap =  new LinkedHashMap<>();
+                //create a list of the readings from the Firebase Query
                 List<Reading> readingList = new ArrayList<>();
+                //To be used for the uniqueWifiMaps count, the format is "BSSID<count>", i.e. BSSID243
                 int wifiCount = 1;
 
-                Log.info("Count: " + dataSnapshot.getChildrenCount());
+                Log.info("Count: " + dataSnapshot.getChildrenCount()); //debug
 
+                //Loop through the datasnapshot list and get all the data
                 for (DataSnapshot readingSnapshot: dataSnapshot.getChildren()) {
                     //region Location
+                    //get the location data
                     LocationLngLat location = new LocationLngLat();
                     if (readingSnapshot.child("location").getChildrenCount() > 0) {
                         location.setAccuracy(Double.parseDouble(readingSnapshot.child("location").child("accuracy").getValue().toString()));
@@ -113,6 +137,7 @@ public class AllReadingServlet extends HttpServlet {
                     //endregion
 
                     //region Magnetometer
+                    //get the magnetometer data
                     Magnetometer magnetometer = new Magnetometer();
                     if (readingSnapshot.child("magnetometer").getChildrenCount() > 0) {
                         magnetometer.setX(Double.parseDouble(readingSnapshot.child("magnetometer").child("x").getValue().toString()));
@@ -122,6 +147,7 @@ public class AllReadingServlet extends HttpServlet {
                     //endregion
 
                     //region RotationMeter
+                    //get the rotation meter data
                     RotationMeter rotationMeter = new RotationMeter();
                     if (readingSnapshot.child("rotationmeter").getChildrenCount() > 0) {
                         rotationMeter.setX(Double.parseDouble(readingSnapshot.child("rotationmeter").child("x").getValue().toString()));
@@ -131,6 +157,7 @@ public class AllReadingServlet extends HttpServlet {
                     //endregion
 
                     //region Accelerometer
+                    //get the accelerometer data
                     Accelerometer accelerometer = new Accelerometer();
                     if (readingSnapshot.child("accelerometer").getChildrenCount() > 0){
                         accelerometer.setX(Double.parseDouble(readingSnapshot.child("accelerometer").child("x").getValue().toString()));
@@ -140,6 +167,7 @@ public class AllReadingServlet extends HttpServlet {
                     //endregion
 
                     //region PhoneInfo
+                    //Get the user's device information
                     PhoneInfo phoneInfo = new PhoneInfo();
                     if (readingSnapshot.child("user").getChildrenCount() > 0){
                         phoneInfo.setModel(readingSnapshot.child("user").child("model").getValue().toString());
@@ -149,8 +177,8 @@ public class AllReadingServlet extends HttpServlet {
                     }
                     //endregion
 
-                    //map of unique bssid with BSSID<count>
                     //region Wi-Fi List
+                    //map of unique bssid with "BSSID<count>"
                     List<WifiItem> wifiList = new ArrayList<>();
                     if (readingSnapshot.child("wifilist").getChildrenCount() > 0){
                         for (DataSnapshot wifi: readingSnapshot.child("wifilist").getChildren()) {
@@ -162,56 +190,62 @@ public class AllReadingServlet extends HttpServlet {
                             //execute this only if format is CSV type and is not already contained in the hashmap
                             if (formatParameter.equals(CSV_TYPE) &&
                                     !uniqueWifisMap.containsKey(wifiItem.getBssid())) {
+                                //add the Bssid as key and an abstract BSSID<count> as value
                                 String uniqueBssid = "BSSID" + wifiCount;
                                 uniqueWifisMap.put(wifiItem.getBssid(), uniqueBssid);
-                                Log.info(uniqueWifisMap.get(wifiItem.getBssid()) + " -- " + wifiItem.getBssid());
+                                //increment count for the next unique hash
                                 wifiCount++;
                             }
                         }
                     }
                     //endregion
 
-                    //region Reading
+                    //region Timestamp
+                    //get the timestamp
                     long timestamp = Long.parseLong(readingSnapshot.child("timestamp").getValue().toString());
 
                     Reading reading = new Reading(timestamp, wifiList, location,
                             accelerometer, magnetometer, rotationMeter, phoneInfo);
                     //endregion
 
+                    //add to the list of the Reading class
                     readingList.add(reading);
-
-                    count++;
                 }
 
 
+                //region Write to Web Page
                 if (formatParameter.equals(CSV_TYPE)){
+                    //if format is CSV
                     try {
+                        //set web page header
                         resp.setHeader("Accept", "text/csv");
                         resp.setHeader("Content-type", "text/csv");
                         resp.setHeader("Content-Disposition","inline; filename=all_readings.csv");
+                        //output csv header
                         resp.getWriter().print(csvHeader(uniqueWifisMap));
+                        //output all the rows in csv
                         resp.getWriter().print(convertToCSV(readingList, uniqueWifisMap));
                     } catch (IOException e){
                         e.printStackTrace();
                     }
 
                 } else{
+                    //default format is JSON
                     Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                    //region Write to Web Page
                     try {
+                        //set web page header
                         resp.setHeader("Accept", "application/json");
                         resp.setHeader("Content-type", "application/json");
                         resp.setHeader("Content-Disposition","inline; filename=all_readings.json");
-                        String json = "{ \"readings\" :" + gson.toJson(readingList) + "}";
-                        resp.getWriter().print(json);
+                        //out the data in JSON
+                        resp.getWriter().print("{ \"readings\" :" + gson.toJson(readingList) + "}");
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    //endregion
-
                 }
+                //endregion
 
-
+                //background thread job is down so release the latch
                 countDownLatch.countDown();
             }
 
@@ -222,7 +256,7 @@ public class AllReadingServlet extends HttpServlet {
         });
 
         try {
-            //wait for firebase to query record.
+            //waits for firebase to query record.
             countDownLatch.await();
         } catch (InterruptedException ex) {
             ex.printStackTrace();
@@ -234,42 +268,41 @@ public class AllReadingServlet extends HttpServlet {
     /**
      * Loop through the reading list
      * Output the reading values accordingly
-     * TODO: check the position of the wifi bssid, else set to 0.0
+     * Check the position of the wifi bssid, else set to 0.0
      * @param readingList
      * @return
      */
     public String convertToCSV(List<Reading> readingList, Map<String, String> uniqueReadingMap){
-        //if error occurred return empty string
-        if (uniqueReadingMap == null) return "";
-        if (readingList == null) return "";
+        //if error occurred return no data error string
+        if (uniqueReadingMap == null || readingList == null) return "error: no data";
 
         //build the output
         StringBuilder output = new StringBuilder();
+        //loop through each reading, build the row and return the whole thing as string
         for (Reading reading: readingList) {
-            List<WifiItem> wifiList = reading.getWifilist();
             //compare it with the unique wifi hash map
-            //check if the wifi is there, if not set to 0.0
+            //check if the wifi is there, if not set to 0
             Integer[] wifiLevelArray = new Integer[uniqueReadingMap.size()];
-
-            //set everything to zero
+            //set everything to zero first
             for (int i = 0; i < wifiLevelArray.length; i++){
                 wifiLevelArray[i] = 0;
             }
 
-            for (WifiItem wifi : wifiList) {
+            //we need to set the level values of the wifis
+            for (WifiItem wifi : reading.getWifilist()) {
                 String bssid = uniqueReadingMap.get(wifi.getBssid());
                 int count = Integer.parseInt(bssid.substring(5, bssid.length()));
                 //Since we start with 1 for the BSSID<count>, we need to remove 1
                 wifiLevelArray[count - 1] = wifi.getLevel();
-                Log.info("bssid [" + count + "] " + wifi.getLevel());
-
             }
-            //build each row
+            //build the wifi level row first
             StringBuilder levelBuilder = new StringBuilder();
             for (Integer level: wifiLevelArray){
                 String levelStr = level + ", ";
                 levelBuilder.append(levelStr);
             }
+
+            //build each row completely
             String out =
                     //user's device information
                     reading.getUser().getDevice() + ", " + reading.getUser().getModel() + ", " +
@@ -287,30 +320,35 @@ public class AllReadingServlet extends HttpServlet {
 
                     //wifis
                     levelBuilder.toString() +
-                            
+
                     //location
                     reading.getLocation().getLongitude() + ", " + reading.getLocation().getLatitude() + ", " +
                         reading.getLocation().getAccuracy() + "\r\n";
+            //append the row to the output StringBuilder
             output.append(out);
-
         }
+
         return output.toString();
     }
 
+    /**
+     * Returns the csv header with all the unique BSSIDs
+     * @param uniqueWifisMap
+     * @return
+     */
     public String csvHeader(Map<String, String> uniqueWifisMap){
         StringBuilder uniqueWifis = new StringBuilder();
         for (String wifi: uniqueWifisMap.keySet()){
             String bssid = uniqueWifisMap.get(wifi) + ", ";
             uniqueWifis.append(bssid);
         }
-        String retStr =  "user_device, user_model, user_product, user_sdklevel, " +
+        return "user_device, user_model, user_product, user_sdklevel, " +
                 "acceleration_x, acceleration_y, acceleration_z, " +
                 "magnetic_x, magnetic_y, magnetic_z, " +
                 "rotation_x, rotation_y, rotation_z, " +
                 uniqueWifis.toString() +
                 "location_longitude, location_latitude, location_accuracy\r\n";
-        Log.info(retStr);
-        return  retStr;
+
 
     }
 }
